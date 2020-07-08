0b8cc39a71c0f0899e801df349e4efec7821faee
shafali03 <shaf03sm@gmail.com>
2020-07-08 14:09:06 +0100

    feat: Render home page without crashing and render title

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
+
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('First React component test', () => {
+
+  it('render without crashing', () => {
+    shallow(<App />)
+  })
+})
