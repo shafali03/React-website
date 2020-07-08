6a764ac48a454b61dd5b86dd60b997dc36e7c951
shafali03 <shaf03sm@gmail.com>
2020-07-08 19:52:37 +0100

    style: Added styles to navbar

--- src/App.js
+++ src/App.js
@@ -5,24 +5,25 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import Home from './Pages/Home/Home'
 import About from './Pages/About/About'
 import Contact from './Pages/Contact/Contact'
+import './App.css'
 
 
 function App() {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+      <div className="container">
+        <ul className="navbar">
+          <li className="nav-link">
+            <Link className="nav-link" to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
+          <li className="nav-link">
+            <Link className="nav-link" to="/about">About</Link>
           </li>
-          <li>
-            <Link to="/contact">Contact</Link>
+          <li className="nav-link">
+            <Link className="nav-link" to="/contact">Contact</Link>
           </li>
         </ul>
-        <hr />
+        {/* <hr /> */}
         <Switch>
           <Route exact path="/">
             <Home />
